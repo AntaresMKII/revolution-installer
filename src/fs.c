536b7c1a833ed5b4ca751e9bce6166d7b4da020f
AntaresMKII <abouemyannick@gmail.com>
2022-08-01 09:45:08 -0400

    Added swap support

--- src/fs.c
+++ src/fs.c
@@ -48,10 +48,24 @@ int make_fs (char* fs, char* disk)
     return 0;
 }
 
+int make_swap (char* disk)
+{
+    char* command = "/usr/sbin/mkswap";
+    int pid;
+
+    pid = fork();
+    if (pid ==0) {
+        execl(command, "revolution-mkswap", disk);
+    }
+
+    waitpid(pid, NULL, 0);
+    return 0;
+}
+
 int verify_fs (char* fs)
 {
-    char *fs_list = {"ext4", "btrfs", "fat", "exfat", "ext3", "ntfs", "xfs"};
-    int fs_arr_size = 7;
+    char *fs_list = {"ext4", "btrfs", "fat", "exfat", "ext3", "ntfs", "xfs", "swap"};
+    int fs_arr_size = 8;
     int rc = 0;
 
     for (int i = 0; i < fs_arr_size; i++) {
@@ -83,16 +97,25 @@ int fs_loop(p_list *list)
             list_dev();
         }
 
-        printf("Enter a file system to use (c to continue): ");
+        printf("Enter a file system to use (c to continue, l to list supported file systems): ");
         scanf("%s", file_sys);
         if (strcmp(file_sys, "c") == 0) {
             rc = 0;
+        }
+        else if (strcmp(file_sys, "l") == 0) {
+            printf("Supported file systems: ext4, btrfs, fat, exfat, ext3, ntfs, xfs, swap");
         }
         else if (verify_fs(file_sys) != 0) {
             printf("This file system is not supported yet.\n");
         }
         else {
-            make_fs(file_sys, part_path);
+            if (strcmp(file_sys, "swap") != 0) {
+            
+                make_fs(file_sys, part_path);
+            }
+            else {
+                make_swap(part_path);
+            }
 
             curr = (part *) malloc(sizeof(part));
             curr->path = (char *) malloc(strlen(part_path));
