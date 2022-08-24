6433a24343458e6cba817606e1be8641f7a53552
AntaresMKII <abouemyannick@gmail.com>
2022-08-24 13:21:08 -0400

    Removed redundant call to execl

--- src/fs.c
+++ src/fs.c
@@ -40,10 +40,7 @@ int make_fs (char* fs, char* disk)
 
     pid = fork();
     if (pid == 0) {
-        if (strcmp(fs_arg, "vfat") != 0)
-            execl(command, "revolution-mkfs", "-f", fs_arg, disk);
-        else
-            execl(command, "revolution-mkfs", fs_arg, "-F", "32", disk);
+        execl(command, "revolution-mkfs", fs_arg, "-F", "32", disk);
     }
 
     waitpid(pid, NULL, 0);
